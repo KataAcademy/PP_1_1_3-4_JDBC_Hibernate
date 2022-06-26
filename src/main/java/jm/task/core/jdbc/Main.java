b941ebed3912b4b61c89ef8fe815e3206bfb11e6
Dzhabrailov2000 <104578715+Dzhabrailov2000@users.noreply.github.com>
2022-06-27 00:48:01 +0300

    need fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,21 @@
 package jm.task.core.jdbc;
 
+import com.mysql.cj.jdbc.ConnectionImpl;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
+        UserDao userDao = new UserDaoJDBCImpl();
 
+        userDao.createUsersTable();
+
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        userDao.saveUser("Name4", "LastName4", (byte) 38);
     }
 }
