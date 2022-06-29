3d555a98cc16773e6c467cb7a2180ff895f1b316
Dzhabrailov2000 <104578715+Dzhabrailov2000@users.noreply.github.com>
2022-06-29 18:45:24 +0300

    ready4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,57 +8,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection;
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.getInstance().getConnection()) {
+        try {
+            connection = Util.getInstance().getConnection();
+            connection.setAutoCommit(false);
             String CREATE_TABLE_SQL = "create table if not exists USERS " + "(" +
                     "id int not null AUTO_INCREMENT, " +
                     "name varchar(30) not null, " +
                     "lastname varchar(50) not null, " +
                     "age int, primary key (id)" + ")";
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(CREATE_TABLE_SQL);
-            statement.close();
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(CREATE_TABLE_SQL);
+            }
+            connection.commit();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getInstance().getConnection()) {
-            Statement statement = connection.createStatement();
+        try {
+            connection = Util.getInstance().getConnection();
+            connection.setAutoCommit(false);
             String DROP_TABLE = "drop table if exists USERS";
-            statement.executeUpdate(DROP_TABLE);
-            statement.close();
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(DROP_TABLE);
+            }
+            connection.commit();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getInstance().getConnection()) {
+        try {
+            connection = Util.getInstance().getConnection();
+            connection.setAutoCommit(false);
             String INSERT_USER_DATA = "insert into USERS(name, lastname, age) VALUES (?, ?, ?)";
-            PreparedStatement stm = connection.prepareStatement(INSERT_USER_DATA);
-            stm.setString(1, name);
-            stm.setString(2, lastName);
-            stm.setByte(3, age);
-            stm.executeUpdate();
-            stm.close();
+            try (PreparedStatement stm = connection.prepareStatement(INSERT_USER_DATA)) {
+                stm.setString(1, name);
+                stm.setString(2, lastName);
+                stm.setByte(3, age);
+                stm.executeUpdate();
+            }
             System.out.println("User с именем – " + name + " добавлен в базу данных");
+            connection.commit();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getInstance().getConnection()) {
+        try {
+            connection = Util.getInstance().getConnection();
+            connection.setAutoCommit(false);
             String DELETE_USER = "delete from USERS where id = " + id;
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(DELETE_USER);
-            statement.close();
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(DELETE_USER);
+            }
+            connection.commit();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -66,18 +84,23 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (Connection connection = Util.getInstance().getConnection()) {
+        try {
+            connection = Util.getInstance().getConnection();
+            connection.setAutoCommit(false);
             String SELECT_ALL_USERS = "select * from USERS";
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-                String lastName = resultSet.getString("lastName");
-                Byte age = resultSet.getByte("age");
-                users.add(new User(name, lastName, age));
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS)) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    String name = resultSet.getString("name");
+                    String lastName = resultSet.getString("lastName");
+                    Byte age = resultSet.getByte("age");
+                    users.add(new User(name, lastName, age));
+                }
+                preparedStatement.close();
+                resultSet.close();
             }
-            preparedStatement.close();
-            resultSet.close();
+            connection.commit();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -85,7 +108,8 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.getInstance().getConnection()) {
+        try {
+            connection = Util.getInstance().getConnection();
             String TRUNCATE_TABLE = "TRUNCATE TABLE kata.USERS";
             Statement statement = connection.createStatement();
             statement.executeUpdate(TRUNCATE_TABLE);
