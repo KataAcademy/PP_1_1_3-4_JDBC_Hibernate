f82b524fab41f2090afe68aa7364da5d7289afbc
Kazakov Aleksey <jk4zakov@yandex.ru>
2022-06-29 18:26:21 +0300

    hibernate finale

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.Connection;
 import java.sql.Driver;
@@ -11,8 +15,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 //         реализуйте алгоритм здесь
-        //UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        //userDaoJDBC.createUsersTable();
+//        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//        userDaoJDBC.createUsersTable();
 //        userDaoJDBC.saveUser("alex","bolduin", (byte) 1);
 //        userDaoJDBC.saveUser("tony","stark", (byte) 2);
 //        userDaoJDBC.saveUser("pen"," penis", (byte) 3);
@@ -21,15 +25,15 @@ public class Main {
 //
 //
 //        userDaoJDBC.getAllUsers().forEach(System.out::println);
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-        userDaoHibernate.createUsersTable();
+        UserService userDaoHibernate = new UserServiceImpl();
+
 
         userDaoHibernate.saveUser("Milk","Milkovich", (byte)46);
         userDaoHibernate.saveUser("amadey","mocart", (byte)35);
         userDaoHibernate.saveUser("Antonio","Saliery", (byte)75);
         userDaoHibernate.saveUser("donatello","mutant ninja turtles", (byte)1);
         userDaoHibernate.getAllUsers().forEach(System.out::println);
-//        userDaoHibernate.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
 
 
 
