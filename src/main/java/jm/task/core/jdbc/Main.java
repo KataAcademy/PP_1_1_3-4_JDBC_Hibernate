806a82e69d4f4a8720aa849f47a4f0a79648646b
sinenkov <107352549+sinenkov@users.noreply.github.com>
2022-07-29 16:11:20 +0500

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,18 +6,15 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
         userService.createUsersTable();
         userService.saveUser("Name1", "LastName1", (byte) 20);
-        //System.out.println("Пользователь Name1 добавлен");
-        //userService.saveUser("Name2", "LastName2", (byte) 25);
-        //userService.saveUser("Name3", "LastName3", (byte) 31);
-        //userService.saveUser("Name4", "LastName4", (byte) 38);
-        //userService.removeUserById(1);
-        //userService.getAllUsers();
-        //userService.cleanUsersTable();
-        //userService.dropUsersTable();
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,29 +8,29 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    //UserDao userDaoJDBC = new UserDaoJDBCImpl();
-    UserDao userDaoHibernate = new UserDaoHibernateImpl();
+    UserDao userDaoJDBC = new UserDaoJDBCImpl();
+    //UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        userDaoHibernate.createUsersTable();
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDaoHibernate.dropUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDaoHibernate.saveUser(name, lastName, age);
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        userDaoHibernate.removeUserById(id);
+        userDaoJDBC.removeUserById(id);
     }
 
-    public List<User> getAllUsers() {return userDaoHibernate.getAllUsers();
+    public List<User> getAllUsers() {return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDaoHibernate.cleanUsersTable();
+        userDaoJDBC.cleanUsersTable();
     }
 }
