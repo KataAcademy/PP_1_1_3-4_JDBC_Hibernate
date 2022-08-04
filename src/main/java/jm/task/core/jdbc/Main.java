7ee7d3ce967e75454aeaac281459adb1cf8ce712
OlenichevKirill <kirill.olen@yandex.ru>
2022-08-04 11:59:51 +0300

    jdbc commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Kirill","Olenichev", (byte) 28);
+        userService.saveUser("Sergey","Olenichev", (byte) 63);
+        userService.saveUser("Richard","Olenichev", (byte) 9);
+        userService.saveUser("Dmitry","Palcev", (byte) 27);
+
+        userService.removeUserById(2);
+
+        List<User> userList = userService.getAllUsers();
+        System.out.println();
+        for (User user:userList) {
+            System.out.println(user);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String DB_DRIVER ="com.mysql.cj.jdbc.Driver";
+    public static final String DB_URL ="jdbc:mysql://localhost:3306/mybase";
+    public static final String DB_USER_NAME ="root";
+    public static final String DB_USER_PASSWORD ="QWASzx12#";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL,DB_USER_NAME,DB_USER_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+
+        }
+        return connection;
+    }
 }
