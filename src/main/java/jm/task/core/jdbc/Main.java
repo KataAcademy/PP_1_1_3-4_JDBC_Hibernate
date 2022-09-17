fc52fa7bb8456db2454c2e17100c235edf60e77b
Maria Filatova <m.bronskaya@gmail.com>
2022-09-17 21:34:13 +0300

    Исправила реализацию через UserDaoJDBCImpl на реализацию через UserServiceImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl uService = new UserServiceImpl(); // исправила реализацию через UserDaoJDBCImpl на реализацию через UserServiceImpl
+
+        // создаю таблицу
+        uService.createUsersTable();
+
+        // сохраняю юзеров в нее
+        uService.saveUser("Nikita", "Petrov", (byte) 20);
+        uService.saveUser("Denis", "Ivanov", (byte) 22);
+        uService.saveUser("Olga", "Fedorova", (byte) 24);
+        uService.saveUser("Marina", "Naumova", (byte) 26);
+
+        uService.removeUserById(1); // удаляю по id 1
+        uService.getAllUsers();
+        uService.cleanUsersTable();
+        uService.dropUsersTable();
     }
 }
