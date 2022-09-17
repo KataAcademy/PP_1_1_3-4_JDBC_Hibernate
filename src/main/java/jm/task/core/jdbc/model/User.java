da4a79dc989ebf0df555df521cca0de123a2a6b4
Maria Filatova <m.bronskaya@gmail.com>
2022-09-17 21:36:08 +0300

    New commit

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity
+@Table(name="users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     @Column
@@ -18,9 +19,7 @@ public class User {
     @Column
     private Byte age;
 
-    public User() {
-
-    }
+    public User() {}
 
     public User(String name, String lastName, Byte age) {
         this.name = name;
@@ -58,5 +57,10 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User { " + "id = " + id + ", name = " + name + '\'' + ", lastName = " + lastName + '\'' + ", age = " + age + '}';
     }
 }
