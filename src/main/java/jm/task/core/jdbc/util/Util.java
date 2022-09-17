18e40782a45cca914fb514d5e3d54451145ae6be
Maria Filatova <m.bronskaya@gmail.com>
2022-09-17 21:39:37 +0300

    New commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/db_users";
+    private static final String USERNAME = "root";
+    private static final String PASS = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASS);
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        return connection;
+    }
 }
+
