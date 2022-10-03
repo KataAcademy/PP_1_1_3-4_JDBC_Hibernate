295addba27e43215458797752c2c79fc401f973f
KINNELYX <terrykenedy@mail.ru>
2022-10-04 02:39:31 +0400

    Hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,18 @@
 package jm.task.core.jdbc.util;
 
 import java.lang.reflect.InvocationTargetException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
-import com.mysql.jdbc.*;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
 
 public class Util {
@@ -15,6 +22,8 @@ public class Util {
     private static final String PASSWORD = "Sql2022.";
     private static final String CONNECTION_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static Connection connection = null;
+    private static SessionFactory sessionFactory;
+    private static final String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
 
     private Util() {
     }
@@ -31,5 +40,30 @@ public class Util {
             System.err.print("Что-то пошло не так!\nСООБЩЕНИЕ ОБ ОШИБКЕ: " + e);
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, CONNECTION_DRIVER);
+                properties.put(Environment.URL, URL);
+                properties.put(Environment.USER, USERNAME);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.DIALECT, DIALECT);
+                properties.put(Environment.SHOW_SQL, false);
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "update");
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
