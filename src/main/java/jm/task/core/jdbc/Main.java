a7fb22a9f8a42b298cd1b7759239739a939be691
AlexLavrov88 <lavrov_alex@list.ru>
2022-11-30 16:30:55 +0300

    sdasd

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,16 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.saveUser("Ivan", "Carevich", (byte) 26);
+        userService.saveUser("Vasilisa", "premydraya", (byte) 22);
+        userService.saveUser("Olesha", "Popovich", (byte) 31);
+        userService.saveUser("Ilya", "Myrometc", (byte) 38);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/MyDBKata";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "rootroot";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection true");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("Connection false");
+        }
+        return connection;
+    }
 }
