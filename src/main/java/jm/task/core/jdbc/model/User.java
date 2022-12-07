76a44ca69563921fa720a14e320e7a6e0e1ca9e4
Max Paz <makshookah@mail.ru>
2022-12-07 17:51:21 +0300

    Задача 1.1.4

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -59,4 +59,13 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,40 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public  class Util {
     // реализуйте настройку соеденения с БД
+    private static final String userName ="root";
+    private static final String password = "1111";
+    private static final String connectURL = "jdbc:mysql://localhost:3306/task";
+    private static Connection connection;
+
+
+
+
+    public static Connection getConnection()  {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(connectURL, userName, password);
+                System.out.println("getConnection");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("Do not open - Exception!!!");
+            }
+        }
+        return connection;
+    }
+    public static void closeConnection(){
+        if(connection != null) {
+            try {
+                System.out.println("closeConnection");
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("Do not closed - Exception!!!");
+            }
+        }
+    }
+
 }
