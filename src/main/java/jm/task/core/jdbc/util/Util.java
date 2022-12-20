f423b2cecef277cf30e84a2111c9edc45807627b
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-20 20:55:06 +0300

    released Util class for getting connection to db server

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String dbUser = "test";
+    private static final String dbPassword = "test";
+    private static final String dbName = "test";
+    private static final String dbHost = "localhost";
+    private static final String dbPort = "3306";
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            String dbURL = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName;
+            connection = DriverManager.getConnection(dbURL, dbUser, dbPassword);
+            System.out.println("Connection OK");
+        } catch (SQLException e) {
+            System.err.println("Connection ERROR");
+        }
+        return connection;
+    }
+
 }
