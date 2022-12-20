5d1d6a27a12cb495a3f716f3d8bcd5b01288fbff
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-20 20:52:09 +0300

    make connection as final

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 public class UserDaoJDBCImpl implements UserDao {
-    private Connection connection = Util.getConnection();
+    private final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
