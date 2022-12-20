49a06fa8de9e547062ef0f0ec061b03aca369c79
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-20 19:13:15 +0300

    added Service

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,50 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
+    public void createUsersTable() {
+        userDaoJDBC.createUsersTable();
+        System.out.println("Таблица users была создана.");
     }
 
     public void dropUsersTable() {
-
+        userDaoJDBC.dropUsersTable();
+        System.out.println("Таблица users была удалена.");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDaoJDBC.saveUser(name, lastName, age);
+        System.out.println("User с именем " + name + " добавлен в базу данных.");
     }
 
     public void removeUserById(long id) {
-
+        if (getUserById(id) != null) {
+            userDaoJDBC.removeUserById(id);
+            System.out.println("Пользователь с id=" + id + " удалён из базы данных.");
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDaoJDBC.getAllUsers();
+    }
+
+    public User getUserById(long id) {
+        if (userDaoJDBC.getUserById(id) != null) {
+            return userDaoJDBC.getUserById(id);
+        } else {
+            System.err.println("Пользователя с id=" + id + " не существует.");
+            return null;
+        }
     }
 
     public void cleanUsersTable() {
-
+        userDaoJDBC.cleanUsersTable();
     }
+
 }
