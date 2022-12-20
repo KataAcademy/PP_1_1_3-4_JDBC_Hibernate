119686edb6cca92ab864c01bc7301551a431c122
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-20 19:17:07 +0300

    added algorithm to Main class

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserServiceImpl userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Джони", "Мнемоник", (byte) 28);
+        userService.saveUser("Иван", "Сусанин", (byte) 64);
+        userService.saveUser("Барак", "Обама", (byte) 60);
+        userService.saveUser("Димон", "из бумера", (byte) 34);
+
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
     }
 }
