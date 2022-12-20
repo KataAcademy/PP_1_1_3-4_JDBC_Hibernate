282d2e6f04d3c254f366fc58cb10c127bb2b28aa
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-21 00:56:47 +0300

    released method getProperties() for Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,27 +1,40 @@
 package jm.task.core.jdbc.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    private static final String dbUser = "test";
-    private static final String dbPassword = "test";
-    private static final String dbName = "test";
-    private static final String dbHost = "localhost";
-    private static final String dbPort = "3306";
-
+    private static final Properties config = getProperties();
     private static Connection connection = null;
 
     public static Connection getConnection() {
         try {
-            String dbURL = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName;
-            connection = DriverManager.getConnection(dbURL, dbUser, dbPassword);
+            connection = DriverManager.getConnection(
+                    config.getProperty("db.url"),
+                    config.getProperty("db.user"),
+                    config.getProperty("db.password")
+            );
             System.out.println("Connection OK");
         } catch (SQLException e) {
             System.err.println("Connection ERROR");
         }
         return connection;
+    }
+
+    private static Properties getProperties() {
+        Properties properties = null;
+        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            properties = new Properties();
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return properties;
     }
 
 }
