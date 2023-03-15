b258875c03574f2a8917ac89d4f55354a6a7afcb
neobliz1 <bliz174@gmail.com>
2023-03-15 11:04:15 -0400

    JDBC configured, DAO in config process

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,48 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+private Util databaseConnection;
 
+public void createUsersTable() {
+    try (Connection DBConnect = databaseConnection.getConnectionToDB()) {
+        try (Statement statement = DBConnect.createStatement()) {
+            statement.execute("CREATE TABLE IF NOT EXISTS users" +
+                    "(id LONG NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+                    " name STRING," +
+                    " lastName STRING," +
+                    " age BYTE)");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+}
 
-    public void createUsersTable() {
+public void dropUsersTable() {
 
-    }
+}
 
-    public void dropUsersTable() {
+public void saveUser(String name, String lastName, byte age) {
 
-    }
+}
 
-    public void saveUser(String name, String lastName, byte age) {
+public void removeUserById(long id) {
 
-    }
+}
 
-    public void removeUserById(long id) {
+public List<User> getAllUsers() {
+    return null;
+}
 
-    }
+public void cleanUsersTable() {
 
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    public void cleanUsersTable() {
-
-    }
+}
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final String url = "jdbc:mysql://localhost:3306";
+    private final String userName = "ususer";
+    private final String pwd = "UD@~VX%uDOpBF7";
+
+    public Connection getConnectionToDB() throws SQLException {
+        return DriverManager.getConnection(url, userName, pwd);
+    }
 }
