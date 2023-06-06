d7be863327be674b80bb3ca2f0c6057d25387633
Denis Filatov <devatyjdan3@gmail.com>
2023-06-06 19:02:59 +0300

    написал Connection

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws IOException {
+        Util util = new Util();
+        util.getConnection();
+        //  Создание таблицы User(ов)
+        // Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+        // Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+        // Очистка таблицы User(ов)
+        // Удаление таблицы
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+
+    public Connection getConnection() {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
