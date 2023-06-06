6973a7dd29bad08fc70961160210fbb3688d498c
VIT363 <mexx-363@yandex.ru>
2023-06-06 21:07:45 +0300

    DB_JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Иван","Иванов",(byte) 46);
+        userService.saveUser("Петр","Петров",(byte) 19);
+        userService.saveUser("Сидор","Сидоров",(byte) 24);
+        userService.saveUser("Алексей", "Алексеев",(byte) 32);
+
+        System.out.println(userService.getAllUsers().toString());
+
+        userService.cleanUsersTable();
+        //System.out.println(userService.getAllUsers().toString());
+
+        userService.dropUsersTable();
+       // System.out.println(userService.getAllUsers().toString());
     }
 }
