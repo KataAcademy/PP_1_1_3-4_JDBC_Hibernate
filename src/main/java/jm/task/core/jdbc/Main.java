6ba2f210f8f1b62937831c227b9e4b135f0ee0b9
zlayapsina <alex.tsaregorodcev1@gmail.com>
2023-10-17 16:25:58 +0300

    задача сделана

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,26 +2,30 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+        Util.getSessionFactory();
         UserService userService = new UserServiceImpl();
 
-        userService.createUsersTable();
 
-        userService.saveUser("Name1", "LastName1", (byte) 20);
-        userService.saveUser("Name2", "LastName2", (byte) 25);
-        userService.saveUser("Name3", "LastName3", (byte) 31);
-        userService.saveUser("Name4", "LastName4", (byte) 38);
-
-        userService.removeUserById(1);
-
-        System.out.println(userService.getAllUsers());
-
-        userService.cleanUsersTable();
-
-        userService.dropUsersTable();
+//        userService.createUsersTable();
+//
+//        userService.saveUser("Name1", "LastName1", (byte) 20);
+//        userService.saveUser("Name2", "LastName2", (byte) 25);
+//        userService.saveUser("Name3", "LastName3", (byte) 31);
+//        userService.saveUser("Name4", "LastName4", (byte) 38);
+//
+//        userService.removeUserById(1);
+//
+//        System.out.println(userService.getAllUsers());
+//
+//        userService.cleanUsersTable();
+//
+//        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URLFIXED =
             "jdbc:mysql://localhost:3306/ppbd?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "123";
+
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection con;
@@ -19,5 +32,47 @@ public class Util {
         }
         return con;
     }
+
+
+
+//    public static SessionFactory getSessionFactory() {
+//        if (sessionFactory == null) {
+//            try {
+//                Configuration config = getConfiguration();
+//                config.addAnnotatedClass(User.class);
+//                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+//                        .applySettings(config.getProperties()).build();
+//                sessionFactory = config.buildSessionFactory(serviceRegistry);
+//                System.out.println("sessionFactory ok");
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//            finally {
+//                assert sessionFactory != null;
+//                sessionFactory.close();
+//            }
+//        }
+//        return sessionFactory;
+//    }
+//
+//    private static Configuration getConfiguration() {
+//        Configuration config = new Configuration();
+//
+//        Properties settings = new Properties();
+////        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+//        settings.put(Environment.URL, URLFIXED);
+//        settings.put(Environment.USER, USERNAME);
+//        settings.put(Environment.PASS, PASSWORD);
+//        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+//
+//        settings.put(Environment.SHOW_SQL, "true");
+//
+//        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+//
+//        settings.put(Environment.HBM2DDL_AUTO, "");
+//
+//        config.setProperties(settings);
+//        return config;
+//    }
 }
 
