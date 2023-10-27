9ea5db7e29b30196e3d82a3182867cd6e084fdf9
Feelan <panfilovich1@gmail.com>
2023-10-27 04:39:50 +0300

    изменение UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,38 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
 
+    public UserServiceImpl() {
+    }
+
+    public void createUsersTable() {
+        daoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        daoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        daoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        daoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return daoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        daoJDBC.cleanUsersTable();
     }
 }
