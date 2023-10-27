7a39ae3dab8633a7e5b4a39e87073e84b7ca2bf9
Feelan <panfilovich1@gmail.com>
2023-10-27 22:18:16 +0300

    изменение UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -7,7 +8,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
+    UserDao daoJDBC = new UserDaoJDBCImpl();
 
     public UserServiceImpl() {
     }
